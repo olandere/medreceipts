6fea254b5d06625cd9132d803b67ce0f11915002
Eric Olander <olandere@gmail.com>
2011-01-07 22:14:40 -0700

    more files

---
+++ test/unit/net/aesw/forhome/PersonControllerTests.groovy
@@ -0,0 +1,17 @@
+package net.aesw.forhome
+
+import grails.test.*
+
+class PersonControllerTests extends ControllerUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
