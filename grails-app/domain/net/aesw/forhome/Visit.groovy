34dc48198e2d50482dd92c416201d171f81119dc
Eric Olander <olandere@gmail.com>
2011-01-07 22:11:35 -0700

    initial commit

---
+++ grails-app/domain/net/aesw/forhome/Visit.groovy
@@ -0,0 +1,15 @@
+package net.aesw.forhome
+
+class Visit {
+
+	Date dateOfVisit
+	Provider provider
+	Person person
+	Float copay
+	Boolean paidWithFSA
+	Boolean countMilage
+	Boolean entryComplete
+	
+    static constraints = {
+    }
+}
