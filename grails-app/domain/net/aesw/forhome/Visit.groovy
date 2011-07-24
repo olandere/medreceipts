d404b3b114160ac0943f0896fca8edb3167e73ea
olandere <olandere@localhost>
2011-07-24 12:52:23 -0700

    updates

--- grails-app/domain/net/aesw/forhome/Visit.groovy
+++ grails-app/domain/net/aesw/forhome/Visit.groovy
@@ -7,14 +7,21 @@ class Visit {
 	Person person
 	Float copay
 	Boolean paidWithFSA
+	Boolean paidWithHSA
 	Boolean countMilage
 	Boolean entryComplete
+	String note
 	
     static constraints = {
 		dateOfVisit(blank: false, unique:['provider','person','copay'])
 		provider(blank: false)
 		person(blank: false)
+		copay(blank: false)
+		paidWithFSA(nullable:false)
+		paidWithFSA validator: {val, obj ->
+			if (val && obj.properties['paidWithHSA']){ return ['visit.error.only.one.paid.flag.can.be.set']} }
+		paidWithHSA(nullable:false)
     }
 	
-	String toString() { "On ${dateOfVisit}, ${person} visited ${provider}, paid ${copay}, FSA: ${paidWithFSA}, complete: ${entryComplete}" }
+	String toString() { "On ${dateOfVisit}, ${person} visited ${provider}, paid ${copay}, FSA: ${paidWithFSA}, HSA: ${paidWithHSA}, complete: ${entryComplete}" }
 }
--- grails-app/views/summaryReport/index.gsp
+++ grails-app/views/summaryReport/index.gsp
@@ -5,13 +5,13 @@
     </head>
     <body>
     	<div class="nav">
-            <span class="menuButton"><a class="home" href="\${createLink(uri: '/')}"><g:message code="default.home.label"/></a></span>
+            <span class="menuButton"><a class="home" href="${createLinkTo(dir:'')}"><g:message code="default.home.label"/></a></span>
         </div>
     	<formset>
 		<legend>Create Summary Report For Year</legend>
 		<g:form action="results">
 			<label for="year">Year</label>
-			<g:textField name="year" />
+			<g:select name="year" from="${['2010', '2011', '2012']}" />
 			<g:submitButton name="search" value="Create"/>
 		</g:form>
 		</formset>
